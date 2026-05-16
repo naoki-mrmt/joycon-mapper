a558799966a1d1c2eb303b7a5126df5c4040cd06
Naoki Muramoto <foxhound@FOXHOUNDPro14-M1MAX.local>
2026-05-16 09:13:24 +0900

    Prepare v0.8.0 release

--- packaging/homebrew/joycon-mapper.rb
+++ packaging/homebrew/joycon-mapper.rb
@@ -1,6 +1,6 @@
 cask "joycon-mapper" do
-  version "0.5.0"
-  sha256 "a1286db36c68af0a4eaaf52147a07f54068b0e7cd1dde2735434bd4591a43f13"
+  version "0.8.0"
+  sha256 "657e417ecb7f9f1c54148c7b51dbee65ba4094c78fab433ba240ff7944556d97"
 
   url "https://github.com/naoki-mrmt/joycon-mapper/releases/download/v#{version}/JoyconMapper-v#{version}.zip"
   name "Joycon Mapper"
